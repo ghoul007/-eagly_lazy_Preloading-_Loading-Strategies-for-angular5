d883dad718dab0c8a3a42cc6654b1b7ca6d88746
ahmed <ghoul.ahmed5@gmail.com>
2017-11-23 11:40:50 +0100

    eager loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+import { ShopModule } from './shop/shop.module';
+import { RouterModule, ROUTES } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import { ShopModule} from './shop/shop.module';
 import { AppComponent } from './app.component';
+import { AppCustomPreloader } from './app-routing-loader';
+import { HomeComponent } from './home/home/home.component';
+
+
+export const routes = [   
+{ path: '', component: HomeComponent, pathMatch: 'full' },
+{ path: '**', component: HomeComponent }
+ ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule , RouterModule.forRoot(routes) ,ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shop/shop.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Router } from '@angular/router'; 
+import { CommonModule } from '@angular/common';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ConfirmComponent } from './confirm/confirm.component';
+
+
+export const routes = [     
+  { path: 'shop', component: CartComponent },     
+  { path: 'shop/checkout', component: CheckoutComponent },    
+  { path: 'shop/confirm', component: ConfirmComponent } 
+];
+
+
+@NgModule({
+  imports: [
+    CommonModule , RouterModule.forChild(routes)
+  ],
+  declarations: [CartComponent, CheckoutComponent, ConfirmComponent]
+})
+export class ShopModule { }
